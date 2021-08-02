4843ff998fbaba33a4f0612712d6fbc81854d438
scoutreilly <scouty33@gmail.com>
2021-08-01 22:18:39 -0600

    working build of character component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+
+import React from "react";
+
+function Character(props) {
+  console.log(props);
+  const characterList = props.map((human) => {
+    return (
+      <div>
+        <h1>hello</h1>
+      </div>
+    );
+  });
+  return characterList;
+}
+
+export default Character;
