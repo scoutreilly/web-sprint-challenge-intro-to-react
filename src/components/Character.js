3bf7d6b7f441f471dab11b7bff2c079c0ed7ba9d
scoutreilly <scouty33@gmail.com>
2021-08-01 22:58:57 -0600

    implementing styled components

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Homeworld from "./Homeworld.js";
+import Div from "./Div.js";
 
 function Character(props) {
   // console.log(props);
@@ -10,18 +11,20 @@ function Character(props) {
   // this is mindblowing, deconstructing props here allows me to map them because it actually exists as an array
   const characterList = propDeconstructed.map((human) => {
     return (
-      <div>
-        <h1>I am {human.name}</h1>
+      <Div>
+        <h1>
+          I am <br></br>
+          {human.name}
+        </h1>
         <h4>Player Stats</h4>
         <p>{human.gender}</p>
         <p>Birth Year ~ {human.birth_year}</p>
         <p>Hair Color ~ {human.hair_color}</p>
         <p>Eye Color ~ {human.eye_color}</p>
-        <p>Height ~ {human.height}</p>
-        <p>Mass ~ {human.mass}</p>
-        {console.log(human.homeworld)}
+        <p>Height ~ {human.height} cm</p>
+        <p>Mass ~ {human.mass} kg</p>
         <Homeworld home={human.homeworld} />
-      </div>
+      </Div>
     );
   });
   return characterList;
---
+++ src/components/Div.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Div = styled.div`
+  height: 100%;
+  margin: 1% 40%;
+  padding: 10px;
+  border: 1px solid #b0a9a5;
+  border-radius: 16px;
+  margin-top: none !important;
+  margin-bottom: none !important;
+`;
+
+export default Div;
